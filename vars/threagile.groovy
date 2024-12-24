8475becf431b907b972d01cd762afb7d42d43b43
Livingstone <95464726+Jymoo@users.noreply.github.com>
2024-12-24 21:31:45 +0300

    Update threagile.groovy

--- vars/threagile.groovy
+++ vars/threagile.groovy
@@ -9,29 +9,29 @@ def call(Map params = [:]) {
 
 
      // Check if Docker is installed
-    def dockerInstalled = false
-    try {
-        sh(script: "docker --version", returnStatus: true)
-        dockerInstalled = true
-    } catch (Exception e) {
-        echo "Docker is not installed. Installing Docker..."
-    }
+    // def dockerInstalled = false
+    // try {
+    //     sh(script: "docker --version", returnStatus: true)
+    //     dockerInstalled = true
+    // } catch (Exception e) {
+    //     echo "Docker is not installed. Installing Docker..."
+    // }
     
-    // Install Docker if it's not installed
-    if (!dockerInstalled) {
-        // For Ubuntu/Debian-based systems
-        sh '''
-            sudo apt-get update
-            sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-            curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-            echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-            sudo apt-get update
-            sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-        '''
-        // Start Docker service
-        sh 'sudo systemctl enable docker'
-        sh 'sudo systemctl start docker'
-    }
+    // // Install Docker if it's not installed
+    // if (!dockerInstalled) {
+    //     // For Ubuntu/Debian-based systems
+    //     sh '''
+    //         sudo apt-get update
+    //         sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+    //         curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    //         echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    //         sudo apt-get update
+    //         sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+    //     '''
+    //     // Start Docker service
+    //     sh 'sudo systemctl enable docker'
+    //     sh 'sudo systemctl start docker'
+    // }
 
 
 
