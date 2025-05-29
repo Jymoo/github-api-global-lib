0ddf9a6ce1b228bcf5ad18b0361e3b6d9efead91
Livingstone <95464726+Jymoo@users.noreply.github.com>
2025-05-29 08:22:48 +0000

    update docker run

--- vars/RunDocker.groovy
+++ vars/RunDocker.groovy
@@ -1,7 +1,13 @@
 def call() {
     echo "Running Docker image..."
     sh '''
+        echo "Freeing port 7000 if it's in use..."
+        container_id=$(docker ps -q --filter "publish=7000")
+        if [ -n "$container_id" ]; then
+            echo "Killing container using port 7000..."
+            docker rm -f $container_id
+        fi
         docker rm -f lendy_container || true
-        docker run -d --name lendy_container -p 8080:8080 $IMAGE_NAME
+        docker run -d --name lendy_container -p 7000:8080 $IMAGE_NAME
     '''
 }
